87a8733b3842784291871f72eb9b4b91f3fbb88a
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-09-22 15:49:08 +0100

    green test -user can see a note

---
+++ src/NoteList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class NoteList extends Component {
+
+  render(){
+    return(
+      <div className="note-list">
+        A test note!
+      </div>
+
+    )
+  }
+
+}
+
+export default NoteList
